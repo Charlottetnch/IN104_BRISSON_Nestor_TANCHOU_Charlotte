ff45daab2a42c02f9651f7c2bd2c1bfef7851938
Nesty85100 <ness.brisson@gmail.com>
2022-04-19 16:54:07 +0200

    test

--- pacman.c
+++ pacman.c
@@ -7,10 +7,31 @@
 #define GHOSTS 4
 
 struct coordinates{int x;
-			 int y;};
+			 	   int y;};
 
-struct pacman{struct coordinates coord;
-			  int nx;
-			  int ny;
-			  
-				}
+struct pacman{struct coordinates p_coor;
+			  int p_nx;    //Direction the player is going
+			  int p_ny;
+			  int lives;   //Nb of lives
+			  int food;	   //Nb of lives
+			  bool p_chase;};   //Chasing or not
+
+struct ghost{struct coordinates g_coor;
+			  int g_nx;  
+			  int g_ny;
+			  bool g_chase;};
+
+
+
+struct ghost* initiate_ghost()
+{
+	struct ghost* list_ghost=malloc(GHOSTS*sizeof(struct ghost));
+	return list_ghost;
+}
+
+/*
+struct initiate_pacman()
+{
+
+}
+*/

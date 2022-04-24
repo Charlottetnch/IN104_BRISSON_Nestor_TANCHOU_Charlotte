c651ed7b5d876eeff20852419e0d2b9b09bf43ea
Nesty85100 <ness.brisson@gmail.com>
2022-04-24 12:33:44 +0200

    test

--- main.c
+++ main.c
@@ -3,27 +3,39 @@
 #include <stdbool.h> 
 #include <time.h>
 #include "sudoku.h"
-
+//#include "morpion.h"
 
 
 
 
 int main()
 {
-	srand(time(NULL));
-	int grid[DIM][DIM];
-	full_grid(grid);
-	show_grid(grid);
-	remove_elements(grid);
-	
-	
-	while(grid_completed(grid)==1)
+	int game;
+	int test;
+	printf("\nVeuillez choisir votre jeu : ");
+	printf("\n(1) Sudoku ");
+	printf("\n(2) Morpion\n ");
+	test=scanf("%d",&game);
+	if(test==1 && game==1)
 		{
-		seize_number(grid);
+			srand(time(NULL));
+		int grid[DIM][DIM];
+
+		full_grid(grid);
+		remove_elements(grid);
 		show_grid(grid);
-		}
 	
-	return 345;
+	
+		while(grid_completed(grid)==1)
+			{
+				seize_number(grid);
+				show_grid(grid);
+			}
+	
+		return 0;
+		}
+
+	
 }
 
 
--- sudoku.c
+++ sudoku.c
@@ -127,73 +127,87 @@ void seize_number(int grid[DIM][DIM])
 				seize_number(grid);
 			}
 }
-void id_sous_matrice( int I[3], int J[3], int i, int j){
-	if (i<3){
+
+
+void id_sous_matrice( int I[3], int J[3], int i, int j)
+{
+	if (i<3)
+		{
 		I[0] = 0;
 		I[1] = 1; 
 		I[2] = 2;
 		//I = { 0 , 1 , 2 };
-		if (j<3){
+		if (j<3)
+			{
 			J[0] = 0;
 			J[1] = 1; 
 			J[2] = 2;
 			//J = { 0 , 1 , 2 }; 
 			}
-		if ((j>=3)&&(j<6)){
+		if (j>=3 && j<6){
 			J[0] = 3;
 			J[1] = 4; 
 			J[2] = 5;
 			//J = { 3 , 4 , 5 }; 
 			}
-		if (j>=6){
+		if (j>=6)
+			{
 			J[0] = 6;
 			J[1] = 7; 
 			J[2] = 8;
 			//J = { 6 , 7 , 8 }; 
 			}
 		}
-	if ((i>=3)&&(i<6)){
+	else if (i>=3 && i<6)
+		{
 		I[0] = 3;
 		I[1] = 4; 
 		I[2] = 5;
 		//int I[3] = { 3 , 4 , 5 };
-		if (j<3){
+		if (j<3)
+			{
 			J[0] = 0;
 			J[1] = 1; 
 			J[2] = 2;
 			//int J[3] = { 0 , 1 , 2 }; 
 			}
-		if ((j>=3)&&(j<6)){
+		if (j>=3 && j<6)
+			{
 			J[0] = 3;
 			J[1] = 4; 
 			J[2] = 5;
 			//int J[3] = { 3 , 4 , 5 }; 
 			}
-		if (j>=6){
+		if (j>=6)
+			{
 			J[0] = 6;
 			J[1] = 7; 
 			J[2] = 8;
 			//int J[3] = { 6 , 7 , 8 }; 
 			}
 		}
-	if (i>6){
+	else if(i>=6)
+		{
 		I[0] = 6;
 		I[1] = 7; 
 		I[2] = 8;
 		//int I[3] = { 6 , 7 , 8 };
-		if (j<3){
+		if (j<3)
+			{
 			J[0] = 0;
 			J[1] = 1; 
 			J[2] = 2;
 			//int J[3] = { 0 , 1 , 2 }; 
 			}
-		if ((j>=3)&&(j<6)){
+		if (j>=3 && j<6)
+			{
 			J[0] = 3;
 			J[1] = 4; 
 			J[2] = 5;
 			//int J[3] = { 3 , 4 , 5 }; 
 			}
-		if (j>=6){
+		if (j>=6)
+			{
 			J[0] = 6;
 			J[1] = 7; 
 			J[2] = 8;
@@ -201,7 +215,7 @@ void id_sous_matrice( int I[3], int J[3], int i, int j){
 			}
 		
 		}
-		}
+}
 
 
 // on créé une fonction qui vérifie que le nombre ajouté à la case (i,j) fonctionne avec la grille 
@@ -212,27 +226,31 @@ int is_correct(int grid[DIM][DIM], int i, int j, int k)
 	
 	// ON VÉRIFIE LA LIGNE 
 	int c=0; 
-	while ((continu == 1)&&(c<9)){
+	while ((continu == 1)&&(c<9))
+	{
 		/*printf("c=%d\n", c);
 		printf("continu = %d\n", continu) ;
 		printf("%d\n", grid[i][c]);*/
-		if (c!=j && grid[i][c]==k){
+		if (c!=j && grid[i][c]==k)
+			{
 			continu = 0;
 			}
 		c++;
-		}
+	}
 	
 	// ON VERIFIE LA COLONNE 
 	int l = 0; 
-	while ((continu == 1)&&(l<9)){
+	while ((continu == 1)&&(l<9))
+	{
 		/*printf("l=%d\n", l);
 		printf("continu = %d\n", continu) ;
 		printf("%d\n", grid[l][j]);*/
-		if (l!=i && grid[l][j]==k){
+		if (l!=i && grid[l][j]==k)
+			{
 			continu = 0 ; 
 			}
 		l++;
-		}
+	}
 	
 	//ON VERIFIE LA MATRICE 
 	
@@ -248,23 +266,30 @@ int is_correct(int grid[DIM][DIM], int i, int j, int k)
 	printf("%d]\n", J[2]);*/
 	
 	//2. on vérifie la sous matrice
-	int ca = 1 ;  
-	while ((continu == 1)&&(ca<10)){
+	int ca = 0 ;  
+	while ((continu == 1)&&(ca<9))
+	{
 		//printf("continu = %d\n", continu);
 		
-		for (int n = I[0]; n <= I[2]; n++){
-			for (int g = J[0]; g <= J[2]; g++){
+		for (int n = I[0]; n <= I[2]; n++)
+			{
+			for (int g = J[0]; g <= J[2]; g++)
+				{
 				ca ++ ; 
 				/*printf("ca = %d\n", ca);
 				printf("n=%d\n",n);
 				printf("g=%d\n",g);*/
-				if (grid[n][g]==k){
-					continu = 0;}
-					}
-					}
-					}
+				if (grid[n][g]==k)
+					{continu = 0;}
+				}
+			}
+	}
 	return continu ; 
 	}
+
+
+
+
 
 void show_grid(int grid[DIM][DIM])
 {
@@ -283,68 +308,6 @@ void show_grid(int grid[DIM][DIM])
 
 }
 
-
-void full_grid(int grid[DIM][DIM])
-{
-	//On remplit notre grille au préalable avec des 0
-	for(int i=0;i<9;i++)
-	{
-		for(int j=0;j<9;j++)
-		{
-			grid[i][j]=0;
-		}
-	}
-	
-	// Diagonales independantes 
-	diagonales(grid);
-	show_grid(grid);
-	
-	//Remplissage des autres sous-matrices case par case 
-	for ( int a = 0; a<3 ; a++){
-		for ( int b = 0; b<3 ; b++){
-			printf("coucou\n");
-			int I[3]={0,0,0};
-			int J[3]={0,0,0};
-			id_sous_matrice(I, J, a*3, b*3);
-			
-			printf("I=[%d", I[0]);
-			printf(",%d,", I[1]);
-			printf("%d]\n", I[2]);	
-			printf("J=[%d", J[0]);
-			printf(",%d,", J[1]);
-			printf("%d]\n", J[2]);
-			
-			for (int k = I[0]; k<=I[2] ; k++ ){
-				for( int l = J[0]; l<=J[2] ; l++ ){
-					printf("%d\n", k);
-					printf("%d\n", l);
-					int* liste = nb_alea();
-					int index = 0; 
-					printf("coucou2\n");
-					show_grid(grid);
-					while ( grid[k][l] == 0 && index < DIM ){
-						if ( is_correct(grid, k, l, liste[index]) == 1){
-							grid[k][l] = liste[index];}
-						else {
-							index ++; }
-						if ( index == 8 && grid[k][l] == 0){
-							// si la permutaion ne fonctionne pas, on remet la sous-matrice à 0 et on en essaye une nouvelle 
-							for (int m = I[0]; m<=I[2] ; m++ ){
-								for( int n = J[0]; n<=J[2] ; n++ ){
-									grid[m][n] = 0 ; 
-									k = I[0];
-									l = J[0];
-									}}
-						}
-					}
-				}
-			}
-		}
-	}
-}
-														
-				
-
 int grid_completed(int grid[DIM][DIM])
 {
 	for(int i=0;i<DIM;i++)
@@ -357,28 +320,79 @@ int grid_completed(int grid[DIM][DIM])
 	return 0;
 }
 
-/*void show_grid(int grid[DIM][DIM])
+
+void full_grid(int grid[DIM][DIM])
 {
-	for(int i=0;i<9;i++)
+	if(grid_completed(grid)==1)
 	{
-		if(i%3==0 && i!=0) printf("------------\n");
-		for(int j=0;j<9;j++)
+	//On remplit notre grille au préalable avec des 0
+	for(int i=0;i<9;i++)
 		{
-			if(grid[i][j]==0) printf(" ");
-			else printf("%d",grid[i][j]);
-			if((j+1)%3==0) printf("|");
-
+		for(int j=0;j<9;j++)
+			{
+			grid[i][j]=0;
+			}
 		}
-		printf("\n");
+	
+	// Diagonales independantes 
+	diagonales(grid);
+
+	
+	
+	//Remplissage des autres sous-matrices case par case 
+	/*
+	for ( int a = 0; a<3 ; a++)
+		{
+		for ( int b = 0; b<3 ; b++)
+			{
+			//printf("coucou\n");
+			int I[3]={0,0,0};
+			int J[3]={0,0,0};
+			id_sous_matrice(I, J, a*3, b*3);
+			
+			//printf("I=[%d", I[0]);
+			//printf(",%d,", I[1]);
+			//printf("%d]\n", I[2]);	
+			//printf("J=[%d", J[0]);
+			//printf(",%d,", J[1]);
+			//printf("%d]\n", J[2]);
+			*/
+			for (int k = 0; k<=8 ; k++ )
+				{
+				for( int l = 0; l<=8 ; l++ )
+					{
+					//printf("%d\n", k);
+					//printf("%d\n", l);
+					int* liste = nb_alea();
+					int index = 0; 
+					//printf("coucou2\n");
+					//show_grid(grid);
+					while ( grid[k][l] == 0)
+						{
+						if ( is_correct(grid, k, l, liste[index]) == 1)
+							{
+							grid[k][l] = liste[index];
+							}
+						else 
+							{
+							index ++; 
+							}
+						if ( index == 8 && grid[k][l] == 0)
+							{
+								free(liste);
+								full_grid(grid);
+							}
+						}
+					}	
+				}
 	}
-
-}*/
-
-
+}
+			
 
 
 
-
+														
+				
 
 
 
